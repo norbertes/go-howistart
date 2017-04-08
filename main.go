74882aa98d3ff1cba45b00d8b8697314b9f1b26a
Norbert Sienkiewicz <norbert.sienkiewicz@gmail.com>
2017-04-08 23:48:57 +0200

    concurrent calls

--- main.go
+++ main.go
@@ -44,6 +44,56 @@ func getWeather(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+type multiWeatherProvider []weatherProvider
+type weatherProvider interface {
+	temperature(city string) (float64, error) // Kelvins
+}
+
+func (w multiWeatherProvider) temperature(city string) (float64, error) {
+	// Make a channel for temperatures, and a channel for errors.
+	// Each provider will push a value into only one.
+	temps := make(chan float64, len(w))
+	errs := make(chan error, len(w))
+
+	// For each provider, spawn a goroutine with an anonymous function.
+	// That function will invoke the temperature method, and forward the response.
+	for _, provider := range w {
+		go func(p weatherProvider) {
+			k, err := p.temperature(city)
+			if err != nil {
+				errs <- err
+				return
+			}
+			temps <- k
+		}(provider)
+	}
+
+	sum := 0.0
+
+	// Collect a temperature or an error from each provider.
+	for i := 0; i < len(w); i++ {
+		select {
+		case temp := <-temps:
+			sum += temp
+		case err := <-errs:
+			return 0, err
+		}
+	}
+
+	return sum / float64(len(w)), nil
+}
+
+type openWeatherMap struct {
+	apiKey string
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
 func (w openWeatherMap) temperature(city string) (float64, error) {
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + w.apiKey + "&q=" + city)
 	if err != nil {
@@ -60,6 +110,10 @@ func (w openWeatherMap) temperature(city string) (float64, error) {
 
 	log.Printf("openWeatherMap: %s: %.2f", city, d.Main.Kelvin)
 	return d.Main.Kelvin, nil
+}
+
+type weatherUnderground struct {
+	apiKey string
 }
 
 func (w weatherUnderground) temperature(city string) (float64, error) {
@@ -85,40 +139,4 @@ func (w weatherUnderground) temperature(city string) (float64, error) {
 	log.Printf("weatherUnderground: %s: %.2f", city, kelvin)
 
 	return kelvin, nil
-}
-
-func (w multiWeatherProvider) temperature(city string) (float64, error) {
-	sum := 0.0
-
-	for _, provider := range w {
-		k, err := provider.temperature(city)
-		if err != nil {
-			return 0, err
-		}
-
-		sum += k
-	}
-
-	return sum / float64(len(w)), nil
-}
-
-type multiWeatherProvider []weatherProvider
-
-type openWeatherMap struct {
-	apiKey string
-}
-
-type weatherUnderground struct {
-	apiKey string
-}
-
-type weatherData struct {
-	Name string `json:"name"`
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	} `json:"main"`
-}
-
-type weatherProvider interface {
-	temperature(city string) (float64, error) // Kelvins
 }
